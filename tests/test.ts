c2a82f5318f9bcdb9342b8f2e386f4938970612a
Jadon <16850875+wolffshots@users.noreply.github.com>
2023-05-29 23:26:16 +0200

    ci: added initial test workflow

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,31 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('card with rate is visible', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByText('rate')).toBeVisible();
+});
+
+test('card with samples is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('samples')).toBeVisible();
+});
+
+test('card with resolution is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('resolution')).toBeVisible();
+});
+
+test('card with time is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('time')).toBeVisible();
+});
+
+test('card with freq is visible', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByText('freq')).toBeVisible();
+});
+
+test('/about ', async ({ page }) => {
+	await page.goto('/about');
+	await expect(page.getByRole('heading', { name: "About SvelteKit" })).toBeVisible();
 });
