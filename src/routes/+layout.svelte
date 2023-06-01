a474547969b8d668f93be17505b76cf65419630e
Jadon <16850875+wolffshots@users.noreply.github.com>
2023-06-01 23:17:48 +0200

    chore: lint and format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
 
 	$: if (browser && analyticsId) {
-        console.log('[Web Vitals]', 'startings vitals');
+		console.log('[Web Vitals]', 'startings vitals');
 		webVitals({
 			path: $page.url.pathname,
 			params: $page.params,
@@ -16,7 +16,13 @@
 			debug: dev
 		});
 	} else {
-		console.log('[Web Vitals]', 'skipping vitals', browser ? 'running in a browser': 'not running in a browser', 'analyticsId:', analyticsId);
+		console.log(
+			'[Web Vitals]',
+			'skipping vitals',
+			browser ? 'running in a browser' : 'not running in a browser',
+			'analyticsId:',
+			analyticsId
+		);
 	}
 </script>
 
