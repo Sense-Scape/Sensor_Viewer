026b9ef572a678fc9dcf9659a520168fdf4de6e9
Jadon <16850875+wolffshots@users.noreply.github.com>
2023-06-01 12:11:41 +0200

    ci: added deliberate delpoy and fixed navbar test

--- tests/navbar.test.ts
+++ tests/navbar.test.ts
@@ -1,17 +1,34 @@
 import { expect, test } from '@playwright/test';
 
-test('navbar routes when clicked', async ({ page }) => {
-	await page.goto('/');
-	await expect(await page.getByRole("navigation").count()).toBe(1);
-    await page.getByText("About").click();
-    await expect(page.url()).toBe("http://localhost:4173/about");
+test('navbar routes when clicked', async ({ page, baseURL, browser }) => {
+    await page.goto('/');
+    await expect(await page.getByRole("navigation").count()).toBe(1);
 
-    await page.getByText("Home").click();
-    await expect(page.url()).toBe("http://localhost:4173/");
+    await Promise.all([
+        page.waitForEvent('framenavigated'),
+        page.getByText("About").click()
+    ]);
 
-    await page.goBack();
-    await expect(page.url()).toBe("http://localhost:4173/about");
+    await expect(page.url()).toBe(baseURL + "/about");
 
-    await page.goForward();
-    await expect(page.url()).toBe("http://localhost:4173/");
+    await Promise.all([
+        page.waitForEvent('framenavigated'),
+        page.getByText("Home").click()
+    ]);
+
+    await expect(page.url()).toBe(baseURL + "/");
+
+    await Promise.all([
+        page.waitForEvent('framenavigated'),
+        page.goBack()
+    ]);
+
+    await expect(page.url()).toBe(baseURL + "/about");
+
+    await Promise.all([
+        page.waitForEvent('framenavigated'),
+        page.goForward()
+    ]);
+
+    await expect(page.url()).toBe(baseURL + "/");
 });
