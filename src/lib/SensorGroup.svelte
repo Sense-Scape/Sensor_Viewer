6d4db97951bb30e21424de0cfcde28459497d748
Grabt234 <gnorrie12@gmail.com>
2023-11-22 22:33:29 +0200

    !Feat: trying to dynamically add sensor group

---
+++ src/lib/SensorGroup.svelte
@@ -0,0 +1,133 @@
+<script lang="ts" context="module">
+	import Chart from 'chart.js/auto';
+
+	// Time domain vars
+	export let timeSampleRate;
+	export let timeChunkSize;
+
+	// Frequency domain vars
+	export let freqSampleRate;
+	export let freqChunkSize;
+
+	export let TimeDomainChart;
+	export let TimeDomainYValues = [0];
+	export let TimeDomainXValues = [0];
+	export let ctxTime;
+
+	export let FreqDomainChart;
+	export let FreqDomainYValues = [0];
+	export let FreqDomainXValues = [0];
+	export let ctxFreq;
+
+	export function InitialiseTimeGraph() {
+		return (TimeDomainChart = new Chart(ctxTime, {
+			type: 'line',
+			data: {
+				labels: TimeDomainXValues,
+				datasets: [
+					{
+						data: TimeDomainYValues,
+						borderColor: 'red',
+						fill: false
+					}
+				]
+			},
+			options: {
+				legend: { display: false },
+				animation: {
+					// Disable animations
+					duration: 1 // Set the duration to 0 for all animations
+				}
+			}
+		}));
+	}
+
+	export function InitialiseFreqGraph() {
+		return (FreqDomainChart = new Chart(ctxFreq, {
+			type: 'line',
+			data: {
+				labels: FreqDomainXValues,
+				datasets: [
+					{
+						data: FreqDomainYValues,
+						borderColor: 'red',
+						fill: false
+					}
+				]
+			},
+			options: {
+				legend: { display: false },
+				animation: {
+					duration: 1
+				}
+			}
+		}));
+	}
+</script>
+
+<div class-="sensorGroup">
+	<div class="sensorGroupTitle">Test</div>
+	<div class="graphSuperGroup">
+		<div class="graphGroup">
+			<div class="parameterContainer">
+				<p class="parameter">Sample Rate: {timeSampleRate}</p>
+				<p class="parameter">Chunk Size: {timeChunkSize}</p>
+			</div>
+			<div>
+				<canvas class="canvas" bind:this={TimeDomainChart} id="TimeDomainChart" />
+			</div>
+		</div>
+		<div class="graphGroup">
+			<div class="parameterContainer">
+				<p class="parameter">Sample Rate: {freqSampleRate}</p>
+				<p class="parameter">Chunk Size: {freqChunkSize}</p>
+			</div>
+			<div>
+				<canvas class="canvas" bind:this={FreqDomainChart} id="FreqDomainChart" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.sensorGroup {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+	}
+
+	.sensorGroupTitle {
+		text-align: center;
+		width: 100%;
+		height: 10%;
+	}
+	.graphSuperGroup {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		height: 90%;
+	}
+
+	.graphGroup {
+		width: 50%;
+		padding: 10px;
+	}
+	.canvas {
+		max-width: 100%;
+		max-height: 100%;
+		width: 100%;
+	}
+
+	.parameterContainer {
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+	}
+	.parameter {
+		text-align: center;
+		max-width: 100%;
+		max-height: 100%;
+		width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,32 @@
 <script lang="ts">
 	import Chart from 'chart.js/auto';
+	import SensorGroup from '$lib/SensorGroup.svelte';
 	import { onMount } from 'svelte';
 	// Setting up of time domain plot
-	let TimeDomainChart;
-	let TimeDomainYValues = [0];
-	let TimeDomainXValues = [0];
-	let ctxTime;
+	// let TimeDomainChart;
+	// let TimeDomainYValues = [0];
+	// let TimeDomainXValues = [0];
+	// let ctxTime;
 
-	export let timeSampleRate = 'X';
-	export let timeChunkSize = 'X';
+	// export let timeSampleRate = 'X';
+	// export let timeChunkSize = 'X';
 
-	let FreqDomainChart;
-	let FreqDomainYValues = [0];
-	let FreqDomainXValues = [0];
-	let ctxFreq;
+	// let FreqDomainChart;
+	// let FreqDomainYValues = [0];
+	// let FreqDomainXValues = [0];
+	// let ctxFreq;
 
-	export let freqSampleRate = 'X';
-	export let freqChunkSize = 'X';
+	// export let freqSampleRate = 'X';
+	// export let freqChunkSize = 'X';
 
+	let sensorGroup = [
+		{
+			timeSampleRate: 1000,
+			timeChunkSize: 512,
+			FreqDomainChart: SensorGroup.InitialiseTimeGraph(),
+			TimeDomainChart: SensorGroup.InitialiseFreqGraph()
+		}
+	];
 	// Callback function used to connect to the websocket, retrieve data
 	// and update the time domain plot
 	$: {
@@ -31,150 +40,90 @@
 
 	// Create the time domain chart and link mount it to the HTML canvas
 	onMount(() => {
-		const TimeWebSocket = new WebSocket('ws://localhost:10100/DataTypes/TimeChunk');
-		let parsedData = null;
-		let datasets = [];
-
-		ctxTime, (ctxFreq = InitialiseGraphGroup());
-
-		TimeWebSocket.addEventListener('message', async (event) => {
-			// Check if parsed data exists, otherwise parse it once
-			if (!parsedData) {
-				const receivedMessage = event.data;
-				parsedData = JSON.parse(receivedMessage);
-
-				// Create datasets array
-				const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
-				for (
-					let channelIndex = 0;
-					channelIndex < parsedData['TimeChunk']['NumChannels'];
-					channelIndex++
-				) {
-					const dataset = {
-						labels: undefined,
-						data: [],
-						borderColor: colors[channelIndex],
-						fill: false
-					};
-					datasets.push(dataset);
-				}
-			}
-
-			const newData = JSON.parse(event.data)['TimeChunk']['Channels'];
-			const numChannels = JSON.parse(event.data)['TimeChunk']['NumChannels'];
-			for (let channelIndex = 0; channelIndex < numChannels; channelIndex++) {
-				datasets[channelIndex].data = newData[channelIndex];
-			}
-
-			// Update chart data efficiently
-			TimeDomainChart.data.datasets = datasets;
-			TimeDomainChart.data.labels = Array.from({ length: 512 }, (_, index) => index + 1);
-			TimeDomainChart.update();
-
-			if (timeSampleRate !== JSON.parse(event.data)['TimeChunk']['SampleRate']) {
-				timeSampleRate = JSON.parse(event.data)['TimeChunk']['SampleRate'];
-			}
-			if (timeChunkSize !== JSON.parse(event.data)['TimeChunk']['ChunkSize']) {
-				timeChunkSize = JSON.parse(event.data)['TimeChunk']['ChunkSize'];
-			}
-		});
-
-		const FreqWebSocket = new WebSocket('ws://localhost:10100/DataTypes/FFTMagnitudeChunk');
-		let FreqParsedData = null;
-		let freqDatasets = [];
-
-		FreqWebSocket.addEventListener('message', async (event) => {
-			// Check if parsed data exists, otherwise parse it once
-			if (!FreqParsedData) {
-				const receivedMessage = event.data;
-				FreqParsedData = JSON.parse(receivedMessage);
-
-				// Create datasets array
-				const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
-				for (
-					let channelIndex = 0;
-					channelIndex < FreqParsedData['FFTMagnitudeChunk']['NumChannels'];
-					channelIndex++
-				) {
-					const dataset = {
-						labels: undefined,
-						data: [],
-						borderColor: colors[channelIndex],
-						fill: false
-					};
-					freqDatasets.push(dataset);
-				}
-			}
-
-			// Update datasets with new data
-			const newData = JSON.parse(event.data)['FFTMagnitudeChunk']['Channels'];
-			const numChannels = JSON.parse(event.data)['FFTMagnitudeChunk']['NumChannels'];
-			for (let channelIndex = 0; channelIndex < numChannels; channelIndex++) {
-				freqDatasets[channelIndex].data = newData[channelIndex];
-			}
-
-			// Update chart data efficiently
-			FreqDomainChart.data.datasets = freqDatasets;
-			FreqDomainChart.data.labels = Array.from({ length: 512 }, (_, index) => index + 1);
-			FreqDomainChart.update();
-
-			if (freqSampleRate !== JSON.parse(event.data)['FFTMagnitudeChunk']['SampleRate']) {
-				freqSampleRate = JSON.parse(event.data)['FFTMagnitudeChunk']['SampleRate'];
-			}
-			if (freqChunkSize !== JSON.parse(event.data)['FFTMagnitudeChunk']['ChunkSize']) {
-				freqChunkSize = JSON.parse(event.data)['FFTMagnitudeChunk']['ChunkSize'];
-			}
-		});
+		// const TimeWebSocket = new WebSocket('ws://localhost:10100/DataTypes/TimeChunk');
+		// let parsedData = null;
+		// let datasets = [];
+		// 	TimeWebSocket.addEventListener('message', async (event) => {
+		// 		// Check if parsed data exists, otherwise parse it once
+		// 		if (!parsedData) {
+		// 			const receivedMessage = event.data;
+		// 			parsedData = JSON.parse(receivedMessage);
+		// 			// Create datasets array
+		// 			const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
+		// 			for (
+		// 				let channelIndex = 0;
+		// 				channelIndex < parsedData['TimeChunk']['NumChannels'];
+		// 				channelIndex++
+		// 			) {
+		// 				const dataset = {
+		// 					labels: undefined,
+		// 					data: [],
+		// 					borderColor: colors[channelIndex],
+		// 					fill: false
+		// 				};
+		// 				datasets.push(dataset);
+		// 			}
+		// 		}
+		// 		const newData = JSON.parse(event.data)['TimeChunk']['Channels'];
+		// 		const numChannels = JSON.parse(event.data)['TimeChunk']['NumChannels'];
+		// 		for (let channelIndex = 0; channelIndex < numChannels; channelIndex++) {
+		// 			datasets[channelIndex].data = newData[channelIndex];
+		// 		}
+		// 		// Update chart data efficiently
+		// 		TimeDomainChart.data.datasets = datasets;
+		// 		TimeDomainChart.data.labels = Array.from({ length: 512 }, (_, index) => index + 1);
+		// 		TimeDomainChart.update();
+		// 		if (timeSampleRate !== JSON.parse(event.data)['TimeChunk']['SampleRate']) {
+		// 			timeSampleRate = JSON.parse(event.data)['TimeChunk']['SampleRate'];
+		// 		}
+		// 		if (timeChunkSize !== JSON.parse(event.data)['TimeChunk']['ChunkSize']) {
+		// 			timeChunkSize = JSON.parse(event.data)['TimeChunk']['ChunkSize'];
+		// 		}
+		// 	});
+		// 	const FreqWebSocket = new WebSocket('ws://localhost:10100/DataTypes/FFTMagnitudeChunk');
+		// 	let FreqParsedData = null;
+		// 	let freqDatasets = [];
+		// 	FreqWebSocket.addEventListener('message', async (event) => {
+		// 		// Check if parsed data exists, otherwise parse it once
+		// 		if (!FreqParsedData) {
+		// 			const receivedMessage = event.data;
+		// 			FreqParsedData = JSON.parse(receivedMessage);
+		// 			// Create datasets array
+		// 			const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple'];
+		// 			for (
+		// 				let channelIndex = 0;
+		// 				channelIndex < FreqParsedData['FFTMagnitudeChunk']['NumChannels'];
+		// 				channelIndex++
+		// 			) {
+		// 				const dataset = {
+		// 					labels: undefined,
+		// 					data: [],
+		// 					borderColor: colors[channelIndex],
+		// 					fill: false
+		// 				};
+		// 				freqDatasets.push(dataset);
+		// 			}
+		// 		}
+		// 		// Update datasets with new data
+		// 		const newData = JSON.parse(event.data)['FFTMagnitudeChunk']['Channels'];
+		// 		const numChannels = JSON.parse(event.data)['FFTMagnitudeChunk']['NumChannels'];
+		// 		for (let channelIndex = 0; channelIndex < numChannels; channelIndex++) {
+		// 			freqDatasets[channelIndex].data = newData[channelIndex];
+		// 		}
+		// 		// Update chart data efficiently
+		// 		FreqDomainChart.data.datasets = freqDatasets;
+		// 		FreqDomainChart.data.labels = Array.from({ length: 512 }, (_, index) => index + 1);
+		// 		FreqDomainChart.update();
+		// 		if (freqSampleRate !== JSON.parse(event.data)['FFTMagnitudeChunk']['SampleRate']) {
+		// 			freqSampleRate = JSON.parse(event.data)['FFTMagnitudeChunk']['SampleRate'];
+		// 		}
+		// 		if (freqChunkSize !== JSON.parse(event.data)['FFTMagnitudeChunk']['ChunkSize']) {
+		// 			freqChunkSize = JSON.parse(event.data)['FFTMagnitudeChunk']['ChunkSize'];
+		// 		}
+		//});
 	});
 
-	function InitialiseGraphGroup() {
-		ctxTime = document.getElementById('TimeDomainChart');
-		TimeDomainChart = new Chart(ctxTime, {
-			type: 'line',
-			data: {
-				labels: TimeDomainXValues,
-				datasets: [
-					{
-						data: TimeDomainYValues,
-						borderColor: 'red',
-						fill: false
-					}
-				]
-			},
-			options: {
-				legend: { display: false },
-				animation: {
-					// Disable animations
-					duration: 1 // Set the duration to 0 for all animations
-				}
-			}
-		});
-
-		ctxFreq = document.getElementById('FreqDomainChart');
-		FreqDomainChart = new Chart(ctxFreq, {
-			type: 'line',
-			data: {
-				labels: FreqDomainXValues,
-				datasets: [
-					{
-						data: FreqDomainYValues,
-						borderColor: 'red',
-						fill: false
-					}
-				]
-			},
-			options: {
-				legend: { display: false },
-				animation: {
-					duration: 1
-				}
-			}
-		});
-
-		return [ctxTime, ctxFreq];
-	}
-
-	function UpdateGraphGroup(numChannel, channelData, sampleRate, chunkSize) {}
+	// function UpdateGraphGroup(numChannel, channelData, sampleRate, chunkSize) {}
 </script>
 
 <svelte:head>
@@ -201,72 +150,11 @@
 </svelte:head>
 
 <div>
-	<div id="sensors">
-		<div class-="sensorGroup">
-			<div class="sensorGroupTitle">Test</div>
-			<div class="graphSuperGroup">
-				<div class="graphGroup">
-					<div class="parameterContainer">
-						<p class="parameter">Sample Rate: {timeSampleRate}</p>
-						<p class="parameter">Chunk Size: {timeChunkSize}</p>
-					</div>
-					<div>
-						<canvas class="canvas" bind:this={TimeDomainChart} id="TimeDomainChart" />
-					</div>
-				</div>
-				<div class="graphGroup">
-					<div class="parameterContainer">
-						<p class="parameter">Sample Rate: {freqSampleRate}</p>
-						<p class="parameter">Chunk Size: {freqChunkSize}</p>
-					</div>
-					<div>
-						<canvas class="canvas" bind:this={FreqDomainChart} id="FreqDomainChart" />
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<div id="sensors" />
+	{#each sensorGroup as sensor}
+		<sensorGroup {...sensor} />
+	{/each}
 </div>
 
 <style>
-	.sensorGroup {
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		height: 100%;
-	}
-
-	.sensorGroupTitle {
-		text-align: center;
-		width: 100%;
-		height: 10%;
-	}
-	.graphSuperGroup {
-		display: flex;
-		flex-direction: row;
-		width: 100%;
-		height: 90%;
-	}
-
-	.graphGroup {
-		width: 50%;
-		padding: 10px;
-	}
-	.canvas {
-		max-width: 100%;
-		max-height: 100%;
-		width: 100%;
-	}
-
-	.parameterContainer {
-		display: flex;
-		flex-direction: row;
-		width: 100%;
-	}
-	.parameter {
-		text-align: center;
-		max-width: 100%;
-		max-height: 100%;
-		width: 100%;
-	}
 </style>
