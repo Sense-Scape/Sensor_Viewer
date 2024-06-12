82787da3898a6defb2f25f7d3d32e1f93148520a
Grabt234 <gnorrie12@gmail.com>
2024-06-12 17:04:43 +0200

    Fix: Removing extra print

--- src/lib/StatusText.svelte
+++ src/lib/StatusText.svelte
@@ -63,7 +63,6 @@
 		// Start by checking if we have seen this source identifier before
 		let dataIndex = GetIdentifierIndex(StatusParsedData['SystemInfo']['StatName']);
 		UpdateMapData(dataIndex, StatusParsedData['SystemInfo']['StatName'], StatusParsedData);
-		console.log(mapData)
 	}
 
 	let StatWebSocket = null;
